ab751a4d2fab786cae64f520d79790a3bc43a3b3
LeoGx <lgarrigoux@gmail.com>
2022-09-13 15:16:32 +0200

    First migration done.

--- EpicEventsCRM/EpicEventsAPI/models.py
+++ EpicEventsCRM/EpicEventsAPI/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.postgres.fields import CIEmailField
 
 
 class Client(models.Model):
@@ -8,15 +7,7 @@ class Client(models.Model):
 
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
-    email = CIEmailField(
-        "email address",
-        unique=True,
-        max_length=100,
-        help_text="Please enter a valid email address.",
-        error_messages={
-            "unique": "A client already exist with that email.",
-        },
-    )
+    email = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
     mobile = models.CharField(max_length=20)
     company_name = models.CharField(max_length=20)
@@ -51,6 +42,6 @@ class Event(models.Model):
     #     settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     # )
     # event_status = ??????
-    attendees = models.IntegerField(max_length=9)
+    attendees = models.IntegerField()
     event_date = models.DateTimeField(auto_now_add=False)
     notes = models.TextField(max_length=1000)
